13ab5fcfc5a5621b44efe5d64557aaa893c80376
Ismail NGUYEN <me@nguyenismail.com>
2015-10-09 12:28:28 +0200

    initial commit
    Document : subject, syllabus

---
+++ Dev/src/ui/model/Minion.java
@@ -0,0 +1,7 @@
+package ui.model;
+
+/**
+ * Created by Ismail on 09-10-2015.
+ */
+public class Minion {
+}
---
+++ Dev/src/ui/model/SpecialPoint.java
@@ -0,0 +1,37 @@
+package ui.model;
+
+/**
+ * Created by Ismail on 09-10-2015.
+ */
+public class SpecialPoint {
+    private Integer x;
+    private Integer y;
+
+    public SpecialPoint(Integer _x, Integer _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    public SpecialPoint()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setY(Integer _y) {
+        y = _y;
+    }
+
+    public void setX(Integer _x) {
+        x = _x;
+    }
+}
