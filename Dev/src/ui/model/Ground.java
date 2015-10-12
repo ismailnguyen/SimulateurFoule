aa74aa56e0bc03e7182ff5347bcfa6b7b2d82bbd
Ismail NGUYEN <me@nguyenismail.com>
2015-10-12 22:12:21 +0200

    MapUtil will make map from text file

--- Dev/src/ui/model/Ground.java
+++ Dev/src/ui/model/Ground.java
@@ -1,7 +1,25 @@
 package ui.model;
 
+import utils.MapUtil;
+
 /**
  * Created by Ismail on 09-10-2015.
  */
 public class Ground {
+    private Square[][] ground;
+    private Integer length;
+    private Integer width;
+
+    public Ground(Square[][] map)
+    {
+        this.ground = map;
+    }
+
+    public Square[][] getGround() {
+        return ground;
+    }
+
+    public void setGround(Square[][] g) {
+        this.ground = g;
+    }
 }
---
+++ Dev/src/utils/MapUtil.java
@@ -0,0 +1,14 @@
+package utils;
+
+import ui.model.Square;
+
+/**
+ * Created by Ismail on 12-10-2015.
+ */
+public class MapUtil {
+    public static Square[][] mapMaker(String file)
+    {
+
+        return null;
+    }
+}
