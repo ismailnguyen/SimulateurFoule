c4a530f6c7f542cc98bb5cedfc23bebe032530b8
Ismail NGUYEN <me@nguyenismail.com>
2015-10-14 23:20:39 +0200

    add sample fxml view

--- Dev/src/ui/Main.java
+++ Dev/src/ui/Main.java
@@ -1,18 +1,32 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import ui.controller.MainController;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+
         primaryStage.setTitle("Simulateur de foule");
-        primaryStage.setScene(new Scene(root, 800, 400));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/root.fxml"));
+        Parent root = loader.load();
+
+        MainController controller = loader.getController();
+        controller.setMain(this);
+        controller.start(primaryStage);
+
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
--- Dev/src/ui/controller/MainController.java
+++ Dev/src/ui/controller/MainController.java
@@ -1,4 +1,23 @@
 package ui.controller;
 
-public class MainController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import ui.Main;
+
+public class MainController extends Application {
+    @FXML
+    private Label label1;
+
+    private Main main;
+
+    @Override
+    public void start(Stage _primaryStage) throws Exception {
+        //label1.setText("aaa");
+    }
+
+    public void setMain(Main _main) {
+        this.main = _main;
+    }
 }
