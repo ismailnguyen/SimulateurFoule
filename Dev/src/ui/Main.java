3196b9c9d5ff931839ff2922788005f3d9ce8247
Ismail NGUYEN <me@nguyenismail.com>
2015-11-12 00:51:58 +0100

    main launcher

---
+++ Dev/src/ui/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Simulateur de foule
+ * 
+ * Kevin DHORNE, Fabien GAMELIN, Ismaïl NGUYEN
+ * 
+ * ESGI 2015/2016
+ */
+
+package ui;
+
+import ui.view.MaFenetre;
+
+public class Main {
+	
+public static void main(String []args) { 		
+		new MaFenetre();	
+	}
+}
