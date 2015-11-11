c2ba22148e268310362f3b8b6357f017d6e955f5
Ismail NGUYEN <me@nguyenismail.com>
2015-11-12 00:55:45 +0100

    A* algorithm implementation essai

---
+++ Dev/src/utils/MapUtil.java
@@ -0,0 +1,30 @@
+/**
+ * Simulateur de foule
+ * 
+ * Kevin DHORNE, Fabien GAMELIN, Ismaïl NGUYEN
+ * 
+ * ESGI 2015/2016
+ */
+
+package utils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import simulation.algorithm.AStar;
+import simulation.algorithm.AStarComparator;
+import simulation.queue.LinkSimple;
+import simulation.queue.LinkedPriorityQueue;
+import ui.model.Ground;
+import ui.model.SpecialPoint;
+import ui.model.Square;
+import algo.linkedlist.nodes.interfaces.ILinkSimple;
+
+public class MapUtil {
+
+	public static List<Square> search(Ground map, Square source, Square target) {
+
+
+		return null;
+	}
+}
